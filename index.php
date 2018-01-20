4ac717ffac7a494e2adbdc081ffa4c6c4475e957
Alexander Forselius <alexander.forselius@buddhalow.com>
2018-01-20 22:39:01 +0000

    Polished CRT

--- index.php
+++ index.php
@@ -18,9 +18,9 @@ if (isset($_GET['url'])) {
 <html>
     <head>
         <?php if(!isset($_GET['url'])):?> <link rel="stylesheet" href="/css/app.css"><?php endif;?>
-        <link rel="stylesheet" href="/css/style.css">§ur
+        <link rel="stylesheet" href="/css/style.css">
         <?php if (isset($_GET['url'])):?>
-        <link rel="stylesheet" href="/css/crt.css"><?php endif;
+        <link rel="stylesheet" href="/css/crt.css"><?php endif;?>
         <meta property="og:title" content="Cathodify" />
         <meta property="og:type" content="website" />
         <meta property="og:description" content="Watch TV as it was in the past">
@@ -28,14 +28,16 @@ if (isset($_GET['url'])) {
         <meta property="og:url" content="<?php echo $actual_link?>" />
         <meta property="og:image" content="<?php echo (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]"?>/images/tv.jpg">
     </head>
-    <body class="crt" style="">
+    <body class="crt" style="margin: 0pt; padding: 0pt">
         <?php if (isset($_GET['url'])): ?>
         <div style="position: relative">
+            
             <img src="/images/tv.jpg" style="position: absolute; left: 0pt; top: 0pt">
             <div class="crt">
                 <iframe  <?php if ($_GET['format'] == 'oldschool'):?> width="1300" height="1215" style="position: absolute; left: 150pt; top: 28pt; opacity: 0.9" <?php else: ?> width="1000" height="755" style="position: absolute; left: 250pt; top: 200pt;" <?php endif; ?> src="<?php echo $url ?>?playsinline=1" frameborder="0" allow="encrypted-media"></iframe>
             </div>
             <img src="/images/tv_overlay.png" style="pointer-events: none; position: absolute; left: 0pt; top: 0pt; z-index: 9000">
+            <a class="btn btn-default" href="/">&lt; Back to start page</a>
         </div>
         <?php else: ?>
         <form style="position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%)" method="GET" action="/">
